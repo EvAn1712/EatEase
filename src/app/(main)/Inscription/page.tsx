0bf0cd3a193bbe64b468669bcc101292a8b79ff9
Aura Tordjman <tordjmanaura@gmail.com>
2024-06-03 15:57:47 +0200

    Modif

--- src/app/(main)/Inscription/page.tsx
+++ src/app/(main)/Inscription/page.tsx
@@ -1,9 +1,10 @@
 'use client';
-import React, { useState } from 'react';
-import { createUserWithEmailAndPassword, updateProfile, onAuthStateChanged, signOut, signInWithEmailAndPassword } from 'firebase/auth';
+import React, { useState, useEffect } from 'react';
+import { createUserWithEmailAndPassword, updateProfile, onAuthStateChanged } from 'firebase/auth';
 import { auth, useAuthContext } from '../authContext';
 import { useRouter } from 'next/navigation';
-// Assurez-vous que les icônes sont importées correctement
+import Link from 'next/link';
+
 const EyeIcon = () => (
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-eye">
     <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
@@ -12,11 +13,12 @@ const EyeIcon = () => (
 );
 
 const EyeOffIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-eye-off">
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" className="feather feather-eye-off">
     <path d="M17.94 17.94L1 1m17.12 3.88C18.72 6.85 19 9.28 19 12c0 1.12-.08 2.23-.24 3.32M5.07 7.07C4.25 8.43 4 9.97 4 12c0 1.12.08 2.23.24 3.32m9.5 2.34l3.26 3.26a1 1 0 0 0 1.42 0l2-2a1 1 0 0 0 0-1.42L4.24 1.24a1 1 0 0 0-1.41 0l-2 2a1 1 0 0 0 0 1.42z"></path>
     <circle cx="12" cy="12" r="3"></circle>
   </svg>
 );
+
 const SignupForm = () => {
   const me = useAuthContext(); // Retrieve user context
 
@@ -25,26 +27,63 @@ const SignupForm = () => {
   const [registerEmail, setRegisterEmail] = useState("");
   const [registerPassword, setRegisterPassword] = useState("");
   const [passwordVisible, setPasswordVisible] = useState(false);
+  const [error, setError] = useState("");  // Ajout de l'état pour gérer les erreurs
 
   const router = useRouter();
   const [user, setUser] = useState(null);
 
-  onAuthStateChanged(auth, (currentUser) => {
-    //@ts-ignore
-    setUser(currentUser);
-  });
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
 
+    return () => unsubscribe();
+  }, []);
+
+  const validatePassword = (password: string): string | null => {
+    if (password.length < 8) {
+      return "Le mot de passe doit contenir au moins 8 caractères.";
+    }
+    if (!/[A-Z]/.test(password)) {
+      return "Le mot de passe doit contenir au moins une lettre majuscule.";
+    }
+    if (!/[a-z]/.test(password)) {
+      return "Le mot de passe doit contenir au moins une lettre minuscule.";
+    }
+    if (!/[0-9]/.test(password)) {
+      return "Le mot de passe doit contenir au moins un chiffre.";
+    }
+    if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
+      return "Le mot de passe doit contenir au moins un symbole spécial.";
+    }
+    return null;
+  };
   const register = async () => {
+    const emailDomain = registerEmail.split('@')[1];
+    if (emailDomain !== 'epfedu.fr' && emailDomain !== 'epfadmin.fr') {
+      setError("L'adresse e-mail doit se terminer par @epfedu.fr ou @epfadmin.fr");
+      return;
+    }
+ const passwordError = validatePassword(registerPassword);
+    if (passwordError) {
+      setError(passwordError);
+      return;
+    }
     try {
       const userCredential = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword);
-      // Update profile with first name and last name
+      // Mettre à jour le profil avec le prénom et le nom
       await updateProfile(userCredential.user, {
         displayName: `${firstName} ${lastName}`
       });
       console.log("Profil mis à jour avec succès !");
-      router.push('/point-of-sale'); // Redirect on successful registration
+      router.push('/point-of-sale'); // Rediriger après une inscription réussie
     } catch (error) {
-      console.error('Une erreur inattendue s\'est produite', error);
+      if (error.code === 'auth/email-already-in-use') {
+        setError("Un compte est déjà associé à cette adresse e-mail. Veuillez vous connecter.");
+      } else {
+        console.error('Une erreur inattendue s\'est produite', error);
+        setError("Une erreur inattendue s'est produite. Veuillez réessayer.");
+      }
     }
   };
 
@@ -52,6 +91,18 @@ const SignupForm = () => {
     <div className="max-w-md mx-auto mt-8 p-6 bg-gray-100 rounded-lg shadow-md">
       <h2 className="text-2xl font-semibold text-gray-800 mb-4">Inscription</h2>
       <form onSubmit={(event) => { event.preventDefault(); register(); }}>
+        {error && (
+          <div className="mb-4 p-4 bg-red-100 text-red-700 rounded">
+            {error.includes("Veuillez vous connecter") ? (
+              <span>
+                {error.split("Veuillez vous connecter")[0]}
+                <Link href="/Connexion" className="text-blue-500 underline"> Veuillez vous connecter</Link>
+              </span>
+            ) : (
+              <span>{error}</span>
+            )}
+          </div>
+        )}
         <div className="mb-4">
           <label htmlFor="firstName" className="block text-gray-700 font-medium mb-1">Prénom :</label>
           <input type="text" id="firstName" className="w-full px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:border-blue-500" required onChange={(e) => setFirstName(e.target.value)} />
@@ -64,6 +115,7 @@ const SignupForm = () => {
           <label htmlFor="email" className="block text-gray-700 font-medium mb-1">Email :</label>
           <input type="email" id="email" className="w-full px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:border-blue-500" required onChange={(e) => setRegisterEmail(e.target.value)} />
         </div>
+        <label htmlFor="password" className="block text-gray-700 font-medium mb-1">Mot de passe :</label>
         <div className="mb-4 relative flex items-center">
           <div className="flex-grow relative">
             <input type={passwordVisible ? "text" : "password"} id="password1" className="w-full px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:border-blue-500" required onChange={(e) => setRegisterPassword(e.target.value)} />
@@ -72,7 +124,9 @@ const SignupForm = () => {
             </button>
           </div>
         </div>
+        <p className="text-sm text-gray-600 mb-4">Le mot de passe doit contenir au moins 8 caractères, incluant une lettre majuscule, une lettre minuscule, un chiffre et un symbole spécial.</p>
         <button type="submit" className="w-full bg-blue-500 text-white font-semibold py-2 rounded-lg hover:bg-blue-600 transition duration-300">S&apos;inscrire</button>
+
       </form>
     </div>
   );
