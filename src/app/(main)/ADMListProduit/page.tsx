4f3706603d1b43c36af7b593023a7bfc9f0644a3
Evan <evan.chinnaya@epfedu.fr>
2024-06-03 20:56:32 +0200

    Ajout barre de recherche dans ADMListeProduit + trie par ordre

--- src/app/(main)/ADMListProduit/page.tsx
+++ src/app/(main)/ADMListProduit/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect, ChangeEvent, FormEvent } from 'react';
+import React, { useState, useEffect, ChangeEvent } from 'react';
 import { getDatabase, ref, get, remove } from 'firebase/database';
 import { FaTrash } from 'react-icons/fa';
 
@@ -20,6 +20,7 @@ const ProductListAdmin: React.FC = () => {
     const [products, setProducts] = useState<IProduct[]>([]);
     const [menus, setMenus] = useState<{ [key: string]: string }>({});
     const [image, setImage] = useState<File | null>(null);
+    const [searchText, setSearchText] = useState<string>(''); // Ajouter l'état de recherche
 
     const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
         if (e.target.files && e.target.files[0]) {
@@ -78,11 +79,36 @@ const ProductListAdmin: React.FC = () => {
         }
     };
 
+    const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setSearchText(e.target.value);
+    };
+
+    const sortProducts = (products: IProduct[]) => {
+        return products.sort((a, b) => {
+            if (a.typeProduit.toLowerCase() < b.typeProduit.toLowerCase()) return -1;
+            if (a.typeProduit.toLowerCase() > b.typeProduit.toLowerCase()) return 1;
+            if (a.nom.toLowerCase() < b.nom.toLowerCase()) return -1;
+            if (a.nom.toLowerCase() > b.nom.toLowerCase()) return 1;
+            return 0;
+        });
+    };
+
+    const filteredAndSortedProducts = sortProducts(products.filter(product =>
+        product.nom.toLowerCase().includes(searchText.toLowerCase())
+    ));
+
     return (
         <div className="w-4/5 mx-auto py-8">
             <h2 className="text-2xl font-bold mb-4">Liste des produits</h2>
+            <input
+                type="text"
+                placeholder="Rechercher un produit"
+                value={searchText}
+                onChange={handleSearchChange}
+                className="mb-4 p-2 border border-gray-300 rounded-md"
+            />
             <div className="flex flex-col gap-4">
-                {products.map((product: IProduct) => (
+                {filteredAndSortedProducts.map((product: IProduct) => (
                     <div key={product.id} className="flex flex-row border border-gray-300 rounded-md p-4 items-center">
                         <div className="flex flex-col flex-grow">
                             <h3 className="text-xl font-bold">{product.nom}</h3>
@@ -90,9 +116,7 @@ const ProductListAdmin: React.FC = () => {
                             <p><span className="font-bold underline">Description:</span> {product.description}</p>
                             <p><span className="font-bold underline">Type de produit:</span> {product.typeProduit}</p>
                             <p><span className="font-bold underline">Menus:</span> {product.idMenus && Array.isArray(product.idMenus) ? product.idMenus.map(menuId => menus[menuId]).join(', ') : 'Aucun'}</p>
-                            <p><span
-                                className="font-bold underline">Allergènes:</span> {product.allergenes ? product.allergenes.join(', ') : 'Aucun'}
-                            </p>
+                            <p><span className="font-bold underline">Allergènes:</span> {product.allergenes ? product.allergenes.join(', ') : 'Aucun'}</p>
                             <p><span className="font-bold underline">Stock:</span> {product.stock}</p>
                             <button onClick={() => handleDelete(product.id)}
                                     className="p-2 bg-red-500 text-white font-semibold rounded-md hover:bg-red-600 focus:outline-none focus:bg-red-600 my-2 flex items-center justify-center" style={{ maxWidth: '40px', minHeight: '10px' }}>
